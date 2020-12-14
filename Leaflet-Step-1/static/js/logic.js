4b21315808be71b5a2fd77bfdd79aeee456b010d
SSN <vasudhanair@gmail.com>
2020-12-14 14:49:32 -0500

    updated logic.js file with create our map step

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -7,3 +7,14 @@ var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}
   id: "mapbox/streets-v11",
   accessToken: API_KEY
 });
+
+// Create our map, giving it the streetmap to display on load
+var myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+  
+  });
+  
+  streetmap.addTo(myMap);
